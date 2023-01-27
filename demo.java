8307d645b7fb208e44cc54a16f52aeb64ade80fd
joshi-akhila <akhilaj868@gmail.com>
2023-01-27 19:12:05 +0530

    new branchc commit

--- demo.java
+++ demo.java
@@ -5,7 +5,7 @@ class DEMO {
 	int x=10;
 	int y=20;
 	
-	System.out.println("Addition:" +  (x+y));
+	System.out.println("Addition:"  +  (x+y));
 	System.out.println("Substraction:" +  (x-y));
 	System.out.println("Multiplication:" +  x*y);
 	System.out.println("Division:" +  x/y);
@@ -13,5 +13,8 @@ class DEMO {
 }
 	public static void main(String[] args) {
         Testmethod();
+
+
+
 }
 }
